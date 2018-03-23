767373783a0c38a24d0aecffb43bdf890894fa15
jaivonmassena <jaivonmassena@gmail.com>
2018-03-22 22:11:15 -0400

    class almost done

--- src/class.js
+++ src/class.js
@@ -8,7 +8,18 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
-
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(ppsw) {
+    if (this.password === ppsw) {
+      return true;
+    }
+    return false;
+  }
+}
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
 // `Cat` should extend the `Animal` class.
